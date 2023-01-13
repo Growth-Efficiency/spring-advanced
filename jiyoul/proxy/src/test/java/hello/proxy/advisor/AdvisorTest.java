809de8545aa00cbaf4238a9c8285df19fd5740ef
JiYoul <wldbf97@naver.com>
2023-01-13 17:51:34 +0900

    [feat] 프록시 팩토리 포인트컷, 어드바이스 어드바이저 학습

---
+++ jiyoul/proxy/src/test/java/hello/proxy/advisor/AdvisorTest.java
@@ -0,0 +1,25 @@
+package hello.proxy.advisor;
+
+import hello.proxy.common.ServiceImpl;
+import hello.proxy.common.ServiceInterface;
+import hello.proxy.common.advice.TimeAdvice;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.Pointcut;
+import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+
+public class AdvisorTest {
+
+    @Test
+    void advisorTest1() {
+        ServiceInterface target = new ServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        DefaultPointcutAdvisor advisor = new DefaultPointcutAdvisor(Pointcut.TRUE, new TimeAdvice());
+        proxyFactory.addAdvisor(advisor);
+        ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
+
+        proxy.save();
+        proxy.find();
+    }
+
+}
