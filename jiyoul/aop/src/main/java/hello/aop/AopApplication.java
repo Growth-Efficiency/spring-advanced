dab3a572c6998d8e20abcb94493cbc0f80ef2108
JiYoul <wldbf97@naver.com>
2023-02-21 20:09:52 +0900

    [feat] AOP 학습 완료

--- jiyoul/aop/src/main/java/hello/aop/AopApplication.java
+++ jiyoul/aop/src/main/java/hello/aop/AopApplication.java
@@ -3,6 +3,7 @@ package hello.aop;
 import hello.aop.order.aop.AspectV1;
 import hello.aop.order.aop.AspectV2;
 import hello.aop.order.aop.AspectV3;
+import hello.aop.order.aop.AspectV4Pointcut;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,7 +13,8 @@ import org.springframework.context.annotation.Import;
 @SpringBootApplication
 //@Import(AspectV1.class)
 //@Import(AspectV2.class)
-@Import(AspectV3.class)
+//@Import(AspectV3.class)
+@Import(AspectV4Pointcut.class)
 public class AopApplication {
 
 	public static void main(String[] args) {
